890f4b35211de9302de443bbb82ce9a710804b6c
tkl <tluk92@gmail.com>
2022-08-27 16:08:05 -0400

    test

--- src/firebase.js
+++ src/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 // Initialize Provider
