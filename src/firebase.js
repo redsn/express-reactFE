4b22d0e3002459a2eef69c2beedd1c6d0231ae00
tkl <tluk92@gmail.com>
2022-08-27 16:09:12 -0400

    testagain

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCU0F57ob9Jemkj6dKVTfeMFHcIZkOT-OE",
-  authDomain: "people-app-lab.firebaseapp.com",
-  projectId: "people-app-lab",
-  storageBucket: "people-app-lab.appspot.com",
-  messagingSenderId: "1012938012144",
-  appId: "1:1012938012144:web:d92d0a8afcef050d345935"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCU0F57ob9Jemkj6dKVTfeMFHcIZkOT-OE",
+//   authDomain: "people-app-lab.firebaseapp.com",
+//   projectId: "people-app-lab",
+//   storageBucket: "people-app-lab.appspot.com",
+//   messagingSenderId: "1012938012144",
+//   appId: "1:1012938012144:web:d92d0a8afcef050d345935"
+// };
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
