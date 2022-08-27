2c92883f37f126c24157646de18eb9ce15ce9a02
tkl <tluk92@gmail.com>
2022-08-27 16:07:05 -0400

    firebasewhy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
 import {login, logout} from '../firebase';
+import styled from 'styled-components';
 
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 3rem;
+    background-color: ${props => props.user ? 'rebeccapurple' : 'black' };
+    color: white;
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    div:first-of-type{
+        display:flex;
+        align-items: center;
+        div{
+            margin-left: 1 rem;
+        }
+    }
+    @media (min-width: 768px) {
+        justify-content: space-between;
+        padding: 0 1rem;
+    }
+`;
 
 export default function Header (props) {
     return(
         <>
-        <nav className="nav">
+        <StyledHeader user={props.user}>
         <Link to="/">
             <div>People App</div>
         </Link>
@@ -30,7 +54,7 @@ export default function Header (props) {
             {/* <div onClick={logout}>Log out</div>
             <div onClick={login}>Log in</div> */}
         </div>
-        </nav>
+        </StyledHeader>
 
         </>
     )
